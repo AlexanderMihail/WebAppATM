d73dfa2171f5f3dd3e50d8dfe006356fc75a5184
Alexander Mihail <alexandermihail@yahoo.com>
2020-02-25 04:11:47 +0200

    Main WEB page has an input line for a card/account/user number/name and an input line for a pin/password. They are encoded to Base64 and sent to the TestServlet which should soon be re-named Authenticate. On the server-side, the rest parameter is decoded back to card+pin and sent back to the user.

--- src/main/java/org/example/atm/TestServlet.java
+++ src/main/java/org/example/atm/TestServlet.java
@@ -2,11 +2,28 @@ package org.example.atm;
 
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Base64;
+import java.util.Enumeration;
 
 public class TestServlet extends HttpServlet {
-    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException
-    {
-            httpServletResponse.getWriter().print("Servlet");
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        res.setContentType("text/plain");
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            String[] value = req.getParameterValues(paramName);
+            byte[] decodedBytes = Base64.getDecoder().decode(value[0]);
+            String[] decodedStrings = new String(decodedBytes).split(":");
+            String card = decodedStrings[0];
+            String pin = decodedStrings[1];
+            out.write("card:"+card);
+            out.write("\n");
+            out.write("pin:"+pin);
+            out.write("\n");
+        }
+        out.write("Servlet");
     }
 }
 
